ff9ea9ddd8f0838b31eb6c8dd1c1bb7037e21c68
Varun kumar <45607763+kickvire@users.noreply.github.com>
2020-11-21 18:30:22 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+import slides from "./../data";
+import Slides from "./Slides";
+export default function App() {
+  const [index, setIndex] = useState(0);
 
-const App = () => {
+  const n = slides.length;
+  const handleNext = (index) => {
+    if (index === n - 1) {
+      return;
+    }
+    setIndex((prev) => prev + 1);
+  };
+  const handleprev = (index) => {
+    if (index === 0) {
+      return;
+    }
+    setIndex((prev) => prev - 1);
+  };
+  const handleReset = () => {
+    if (index === 0) {
+      return;
+    }
+    let first = 0;
+    setIndex(first);
+  };
+
   return (
-    <></>
-  )
+    <div className="App">
+      <Slides
+        index={index}
+        onRestart={handleReset}
+        onPrev={handleprev}
+        onNext={handleNext}
+        text={slides[index].text}
+        title={slides[index].title}
+      />
+    </div>
+  );
 }
-
-
-export default App;
